c4b8fc3c745d6486603ddf6d03c4cc883529629c
TheInventori <148151506+TheInventori@users.noreply.github.com>
2023-12-20 11:19:46 +0100

    Create main.c

---
+++ 2023-12-20/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// vytvorte program, ktory nahradi v retazci lubovolny znak inym lubovolnym znakom
+// vrati pocet vyskytov lubovolneho retazca
+// pripoki retazec2 na koniec retazca1
+// zmeni vsetky pismena na velke a vytvori z textu schody smerom dole
+
+int main() {
+  return 0;
+}
