49c50631ecacf174bd9410a9dff11e49f33860e8
TheInventori <148151506+TheInventori@users.noreply.github.com>
2023-12-20 11:33:02 +0100

    Update main.c

--- 2023-12-20/main.c
+++ 2023-12-20/main.c
@@ -7,6 +7,28 @@
 // pripoki retazec2 na koniec retazca1
 // zmeni vsetky pismena na velke a vytvori z textu schody smerom dole
 
+
 int main() {
-  return 0;
+	char text[100];
+	char letter1;
+	char letter2;
+	
+	printf("zadaj retazec: ");
+	gets(text);
+	
+	printf("zadaj hladany znak: ");
+	getc(letter1);
+	
+	printf("zadaj nahradny znak: ");
+	getc(letter2);
+	
+	for(int i = 0; i < strlen(text); i++) {
+		if(text[i] == letter1) {
+			text[i] = letter2;
+		}
+	}
+	
+	puts(text);
+	
+	return 0;
 }
